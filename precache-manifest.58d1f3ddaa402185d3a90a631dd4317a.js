f03d319f795066bbdb1acc2297f9376dedcf8139
jessica bradham <jessijoke@yahoo.com>
2020-05-09 03:21:36 -0400

    First Commit

---
+++ precache-manifest.58d1f3ddaa402185d3a90a631dd4317a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253334a54d93efefd4eb1d67d3ae5c90",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "f56d4d97d0bf0fc923d7",
+    "url": "/pizza/static/css/main.7ce20a06.chunk.css"
+  },
+  {
+    "revision": "31a94a2c45d065808742",
+    "url": "/pizza/static/js/2.80eb23cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pizza/static/js/2.80eb23cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56d4d97d0bf0fc923d7",
+    "url": "/pizza/static/js/main.18a217a6.chunk.js"
+  },
+  {
+    "revision": "eb31208452d7e0bf857a",
+    "url": "/pizza/static/js/runtime-main.38031e57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pizza/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c6071e6157292836da094dc0109eda5",
+    "url": "/pizza/static/media/pizza-bg.8c6071e6.jpg"
+  }
+]);
